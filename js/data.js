cbd06b8c61c894ed4249a7ef46634912ae8b04b2
kurtdadedavid <78076748+kurtdadedavid@users.noreply.github.com>
2021-03-25 23:13:13 +0800

    edit

--- js/data.js
+++ js/data.js
@@ -1,4 +1,11 @@
 function togglePopup() {
     document.getElementById("popup-1").classList.toggle("active");
 }
-
+function cal() {
+    //parsing
+    var uinp = parseInt(document.getElementById("price").value);
+    var uinp2 = parseInt(document.getElementById("qty").value);
+    var calc = uinp * uinp2;
+    document.getElementById("subtotal").value = calc.toFixed(2);
+    console.log(calc)
+}
